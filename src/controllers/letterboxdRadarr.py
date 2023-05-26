f4726154b39a1df53a5d9e3728bbe16f6eb88e48
jsavargas <jsavargas@gmail.com>
2023-05-25 22:14:03 -0400

    ThreadPoolExecutor

--- src/controllers/letterboxdRadarr.py
+++ src/controllers/letterboxdRadarr.py
@@ -3,65 +3,35 @@ import os
 import re
 import requests
 from bs4 import BeautifulSoup
+import concurrent.futures
 
 class letterboxdRadarr:
 
     def __init__(self):
         self._version = "1.10.0"
-        self.app_id = 'APP_ID'
+        self.max_workers = 30
 
-    def watchlistv(self):
-
-        data = [{"id":484247,"imdb_id":"tt7040874","title":"A Simple Favor","release_year":"2018","clean_title":"/film/a-simple-favor/","adult":False}
-        ,{"id":557950,"imdb_id":"tt9224288","title":"Mainstream","release_year":"2020","clean_title":"/film/mainstream-2020/","adult":False}
-        ,{"id":204082,"imdb_id":"tt2312718","title":"Homefront","release_year":"2013","clean_title":"/film/homefront/","adult":False}
-        ,{"id":827681,"imdb_id":"tt15496702","title":"The Lost Year 1986","release_year":"2022","clean_title":"/film/the-lost-year-1986/","adult":False}
-        ,{"id":798286,"imdb_id":"tt13521006","title":"Beau Is Afraid","release_year":"2023","clean_title":"/film/beau-is-afraid/","adult":False}
-        ,{"id":840326,"imdb_id":"tt14846026","title":"Sisu","release_year":"2022","clean_title":"/film/sisu-2022/","adult":False}
-        ,{"id":14337,"imdb_id":"tt0390384","title":"Primer","release_year":"2004","clean_title":"/film/primer/","adult":False}
-        ,{"id":569094,"imdb_id":"tt9362722","title":"Spider-Man: Across the Spider-Verse","release_year":"2023","clean_title":"/film/spider-man-across-the-spider-verse/","adult":False}
-        ,{"id":713704,"imdb_id":"tt13345606","title":"Evil Dead Rise","release_year":"2023","clean_title":"/film/evil-dead-rise/","adult":False}
-        ,{"id":502356,"imdb_id":"tt6718170","title":"The Super Mario Bros. Movie","release_year":"2023","clean_title":"/film/the-super-mario-bros-movie/","adult":False}
-        ,{"id":964980,"imdb_id":"tt16419074","title":"Air","release_year":"2023","clean_title":"/film/air-2023/","adult":False}
-        ,{"id":881,"imdb_id":"tt0104257","title":"A Few Good Men","release_year":"1992","clean_title":"/film/a-few-good-men/","adult":False}
-        ]
-        
-        return data
-    
-    def watchlist(self):
+    def watchlist(self,username):
         print('watchlist')
-        username = 'jsavargas'
         watched_movies = []
+        movies = []
         session = requests.Session()
 
-        watchlist_url = "https://letterboxd.com/{}/watchlist/".format(username)
-        
-        search_page = session.get(watchlist_url)
-        #print(f'search_page "{search_page.text}" "{watchlist_url}"')
-        search_soup = BeautifulSoup(search_page.content, "html.parser")
+        count = 0
+        while True:
+            count += 1
 
+            watchlist_url = "https://letterboxd.com/{}/watchlist/page/{}/".format(username, count)
+            search_page = session.get(watchlist_url)
+            search_soup = BeautifulSoup(search_page.content, "html.parser")
+            _movies = search_soup.findAll("div", {"class": "film-poster"})
+            if _movies:
+                for _movie in _movies:
+                    movies.append(_movie)
+            else: break
 
-        #watched_movies = [{'title':movie.get('title'),'accountID':movie.get('accountID')} for movie in movies if movie.get('type') == 'movie' and movie.get('accountID') == '1']
-
-
-        # Obtener el enlace de la película
-        #watched_movies = search_soup.findAll("div", {"class": "film-poster"})
-        #watched_movies = [{'clean_title':movie['data-film-slug'], 'title':movie.find('img')['alt']} for movie in search_soup.findAll("div", {"class": "film-poster"})]
-        for movie in search_soup.findAll("div", {"class": "film-poster"}):
-            details = self.getDetailsMovie(movie['data-film-slug'])
-            watched_movies.append({
-                'id':details['id'],
-                'imdb_id':details['imdb_id'],
-                'title':movie.find('img')['alt'],
-                'release_year':details['release_year'],
-                'clean_title':movie['data-film-slug'], 
-                'themoviedb':details['themoviedb'],
-                'adult': False
-                } )
-            #break
-        print('watchlist')
-        print(f'movie_link  => "{watched_movies}"')
-
+        print(f'movie_link  => "{movies}"')
+        # Realizar las solicitudes en paralelo
         #{"id":484247,
         # "imdb_id":"tt7040874",
         # "title":"A Simple Favor",
@@ -69,36 +39,73 @@ class letterboxdRadarr:
         # "clean_title":"/film/a-simple-favor/",
         # "adult":false}
 
-        return watched_movies
-    
+        return self.concurrent(movies)
+
     def getDetailsMovie(self, movie):
-        print('getDetailsMovie 1')
         imdb_id = ''
         themoviedb = ''
 
-        watchlist_url = f"https://letterboxd.com{movie}"
+        title = movie['data-film-slug']
+        watchlist_url = f"https://letterboxd.com{title}"
 
         session = requests.Session()
         search_page = session.get(watchlist_url)
-        #print(f'search_page "{search_page.text}" "{watchlist_url}"')
-        #search_soup = BeautifulSoup(search_page.content, "html.parser")
 
         IMDB_REGEX = r'http://www\.imdb\.com/title/(tt\d+)/'
         TMDB_REGEX = r'https://www\.themoviedb\.org/movie/(\d+)/'
+        id_pattern = r'filmData.*?id:\s(\d+)'
+        release_year_pattern = r'filmData.*?releaseYear:\s"(\d+)"'
 
-        #watched_movies = search_soup.find("div", {"class": "film-poster"})
         imdb_id_match = re.search(IMDB_REGEX, search_page.text)
         themoviedb_match = re.search(TMDB_REGEX, search_page.text)
-        print('getDetailsMovie 2')
 
         if imdb_id_match: imdb_id = imdb_id_match.group(1)
         if themoviedb_match: themoviedb = themoviedb_match.group(1)
-
-        id_pattern = r'filmData.*?id:\s(\d+)'
-        release_year_pattern = r'filmData.*?releaseYear:\s"(\d+)"'
 
         id_match = re.search(id_pattern, search_page.text)
         release_year_match = re.search(release_year_pattern, search_page.text)
         if id_match: id_value = id_match.group(1)
         if release_year_match: release_year_value = release_year_match.group(1)
-        return {'imdb_id':imdb_id, 'themoviedb':themoviedb, 'id': id_value, 'release_year':release_year_value }
+        return {'imdb_id': imdb_id, 
+                'themoviedb': themoviedb, 
+                'id': id_value, 
+                'title': movie.find('img')['alt'],
+                'release_year': release_year_value, 
+                "clean_title": movie['data-film-slug'] }
+    
+    def films(self,username):
+        print('watchlist')
+        watched_movies = []
+        movies = []
+        session = requests.Session()
+
+        count = 0
+        while True:
+            count += 1
+
+            watchlist_url = "https://letterboxd.com/{}/films/page/{}/".format(username, count)
+            search_page = session.get(watchlist_url)
+            search_soup = BeautifulSoup(search_page.content, "html.parser")
+            _movies = search_soup.findAll("div", {"class": "film-poster"})
+            if _movies:
+                for _movie in _movies:
+                    movies.append(_movie)
+            else: break
+
+        print(f'movie_link  => "{movies}"', flush=True)
+        
+        return self.concurrent(movies)
+
+    def concurrent(self, movies):
+        watched_movies = []
+
+        with concurrent.futures.ThreadPoolExecutor(max_workers=self.max_workers) as executor:
+            # Iterar sobre las URLs y enviar las solicitudes
+            futures = [executor.submit(self.getDetailsMovie, movie) for movie in movies]
+    
+        # Obtener los resultados a medida que se van completando las solicitudes
+        for future in concurrent.futures.as_completed(futures):
+            resultado = future.result()
+            watched_movies.append(resultado)
+
+        return watched_movies
--- src/routes/index.py
+++ src/routes/index.py
@@ -19,22 +19,28 @@ index = Blueprint("index", __name__)
 
 
 @index.route("/<user>/watchlist")
-def group(user=None):
+def watchlist(user=None):
     print(f" [!] /<user>", flush=True)
-
     movies = []
-
     try:
-
         print(f" [!] /<user>", flush=True)
-
         newletterboxdRadarr = letterboxdRadarr()
-        movies = newletterboxdRadarr.watchlist()
-
+        movies = newletterboxdRadarr.watchlist(user)
     except Exception as e:
         print(f" [!] Exception index.route group[{e}]", flush=True)
+    return jsonify(movies)
+    return '¡Hola, mundo!'
 
-
+@index.route("/<user>/films")
+def films(user=None):
+    print(f" [!] /<user>", flush=True)
+    movies = []
+    try:
+        print(f" [!] /<user>", flush=True)
+        newletterboxdRadarr = letterboxdRadarr()
+        movies = newletterboxdRadarr.films(user)
+    except Exception as e:
+        print(f" [!] Exception index.route group[{e}]", flush=True)
     return jsonify(movies)
     return '¡Hola, mundo!'
 
